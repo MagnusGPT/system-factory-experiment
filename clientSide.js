03bd04a3a8e5cf276149ca230c6aa58fc68b823c
Magnus The Developer <95050512+MagnusGPT@users.noreply.github.com>
2026-03-05 19:00:24 +0100

    Worked on shared state connections. Feels more like a system now.

--- clientSide.js
+++ clientSide.js
@@ -3,7 +3,16 @@ const socket = io("http://localhost:3000");
 
 
 let point = 0;
-socket.on("addPoint", (receivedPoint) => {
+socket.on("addPoints", (receivedPoint) => {
     point += receivedPoint;
     console.log(`Your point is now ${point}`);
+});
+
+socket.on("checkPoints", () => {
+    socket.emit("checkPoints", point);
+});
+
+socket.on("setPoints", (pointAmount) => {
+    point = pointAmount;
+    console.log(`Points are set to ${pointAmount}`);
 });
--- index.js
+++ index.js
@@ -1,12 +1,60 @@
 const { Server } = require("socket.io");
+const { connect } = require("socket.io-client");
 const io = new Server(3000);
 
+/*
+List of signals:
+setPoints: sets amount of points of the emitted socket
+addPoints: adds points to the emitted socket
+*/
+
+let amountOfConnections = 0;
+let sockets = {};
+let threshold = 100;
 io.on("connection", (socket) => {
     console.log("Someone connected");
+    amountOfConnections++;
+    sockets[socket.id] = { balance: 0 };
+    console.log(sockets);
+    socket.on("checkPoints", (pointAmount) => {
+        if (pointAmount > threshold) {
+            socket.emit("setPoints", 0);
+            sockets[socket.id].balance = 0;
+            console.log(`Socket ${socket.id} surpassed the threshold. Their points set to 0.`);
+        }
+        else {
+            sockets[socket.id].balance = pointAmount;
+        }
+    });
+
+    socket.on("disconnect", () => {
+        amountOfConnections--;
+        console.log(`Socket ${socket.id} disconnected. Sharing their points between others.`);
+        sharedAmount = Math.floor(sockets[socket.id].balance / amountOfConnections);
+        if (amountOfConnections === 0) { 
+            console.log("No connections left."); 
+        }
+        else {
+            console.emit("addPoints", sharedAmount)
+            console.log(`Everyone received ${sharedAmount}.`);
+        }
+        sockets[socket.id].balance = 0;
+    });
 });
 
-let pointEmitted = 1;
+// Sends a call to check coins.
 setInterval(() => {
-    io.emit("addPoint", pointEmitted);
+    io.emit("checkPoints");
+}, 100);
+
+let pointEmitted = 1;
+// Emit points.
+setInterval(() => {
+    io.emit("addPoints", pointEmitted);
     console.log(`${pointEmitted} amount of points emitted.`);
 }, 1000);
+
+if (pointEmitted > 10) {
+    pointEmitted = 0;
+    console.log(`Point went over 10. Reset to ${pointEmitted}`);
+}
